2e233ac466f0d6a858df3aa8487f34b6e53ae54f
Allpez <alexlop1987@gmail.com>
2024-08-24 04:14:14 -0500

    pagina details

--- scripts/details.js
+++ scripts/details.js
@@ -1,36 +1,119 @@
-const url = window.location.search;
-const urlObjeto = new URLSearchParams(url);
-const eventId = urlObjeto.get('id')
 
-if (eventId) {
-    const event = data.events.find(e => e._id === eventId);
 
-    if (event) {
-        const cardContainer = document.getElementById("card-container");
-        const cardContent = document.createElement("div");
-        cardContent.className = "card2 d-flex";
-        cardContent.innerHTML = `
-        <div class="row align-items-center">
-            <div class="col-md-5 col-12 mb-3 mb-md-0 d-flex justify-content-center">
-                <img class="img-fluid object-fit-cover img_card_detail" src="${event.image}" alt="${event.name}">
-            </div>
-            <div class="col-md-7 col-12">
-                <div class="card-body container-fluid">
-                    <h4 class="name d-card pb-2 rounded bg-success"> ${event.name}</h4>
-                    <ul class="list-group list-group-flush rounded bg-success-subtle flex-grow-1">                        
-                        <li class="list-group-item rounded"><span class="fw-bold">Date: </span>${event.date}</li>
-                        <li class="list-group-item rounded"><span class="fw-bold">Description: </span>${event.description}</li>
-                        <li class="list-group-item rounded"><span class="fw-bold">Category: </span>${event.category}</li>
-                        <li class="list-group-item rounded"><span class="fw-bold">Place: </span>${event.place}</li>
-                        <li class="list-group-item rounded"><span class="fw-bold">Capacity: </span>${event.capacity}</li>
-                        ${event.assistance ? `<li class="list-group-item rounded"><span class="fw-bold">Assistance: </span>${event.assistance} people</li>` : ''}
-                        ${event.estimate ? `<li class="list-group-item rounded"><span class="fw-bold">Estimate: </span>${event.estimate} people</li>` : ''}
-                        <li class="list-group-item rounded"><span class="fw-bold">Price: </span>${event.price} $</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        `;
-        cardContainer.appendChild(cardContent);
-    } 
-} 
+const urlParam = window.location.search;
+const urlObjeto = new URLSearchParams(urlParam);
+const departmentId = urlObjeto.get('id');
+
+let url = "https://api-colombia.com";
+let urlDepartaments = `${url}/api/v1/Department`;
+let urlCities = `${url}/api/v1/Department/${departmentId}/cities`;
+let urlNaturalAreas = `${url}/api/v1/Department/${departmentId}/naturalareas`;
+
+let citiesContainer = document.getElementById("citiesContainer");
+let areasContainer = document.getElementById("areasContainer");
+
+let cities = [];
+let naturalAreas = [];
+
+// Fetch cities
+fetch(urlCities)
+    .then(response => response.json())
+    .then(data => {
+        cities = data;
+        createCityCards(cities);
+    })
+    .catch(error => console.error("Error al obtener los datos de ciudades:", error));
+
+// Fetch natural areas
+fetch(urlNaturalAreas)
+    .then(response => response.json())
+    .then(data => {
+        naturalAreas = data;
+        console.log(naturalAreas);
+        createNaturalAreaCards(naturalAreas);
+    })
+    .catch(error => console.error("Error al obtener los datos de áreas naturales:", error));
+
+// Fetch department details
+fetch(urlDepartaments)
+    .then(response => response.json())
+    .then(departments => {
+        if (departmentId) {
+            const department = departments.find(d => d.id.toString() === departmentId);
+
+            if (department) {
+                const cardContainer = document.getElementById("card");
+                const cardContent = document.createElement("div");
+                cardContent.className = "card2 d-flex";
+                cardContent.innerHTML = `
+                    <div class="row align-items-center">
+                        <div class="col-md-5 col-12 mb-3 mb-md-0 d-flex justify-content-center">
+                            <img class="img-fluid object-fit-cover img_card_detail" src="../imgs/details.jpg">
+                        </div>
+                        <div class="col-md-7 col-12">
+                            <div class="card-body container-fluid">
+                                <h4 class="name d-card pb-2 text-center">${department.name}</h4>
+                                <ul class="list-group list-group-flush rounded bg-success-subtle flex-grow-1">                        
+                                    <li class="list-group-item"><span class="fw-bold">Descripción: </span>${department.description}</li>
+                                    <li class="list-group-item"><span class="fw-bold">Municipios: </span>${department.municipalities}</li>
+                                    <li class="list-group-item"><span class="fw-bold">Superficie: </span>${department.surface} Km<sup>2</sup></li>
+                                    <li class="list-group-item"><span class="fw-bold">Población: </span>${department.population} hab</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                cardContainer.appendChild(cardContent);
+            }
+        }
+    })
+    .catch(error => console.error("Error al obtener los datos del departamento:", error));
+
+function createCityCards(cities) {
+    citiesContainer.innerHTML = `<h2>Ciudades</h2>`;
+
+    if (cities.length > 0) {
+        let header = document.createElement('h2');
+        header.className = 'text-center';
+        header.textContent = 'Áreas Naturales';
+        areasContainer.appendChild(header);
+
+
+        cities.forEach(city => {
+            let card = document.createElement("div");
+            card.className = "card";
+
+            card.innerHTML = `
+            <img class="card-img w-25" src="../imgs/img-card.jpg">
+            <div class="card-body p-1 mt-2">
+                <h5 class="card-name">${city.name}</h5>
+            </div>`;
+
+            citiesContainer.appendChild(card);
+        });
+    }
+}
+
+function createNaturalAreaCards(naturalAreas) {
+    areasContainer.innerHTML = '';
+
+    if (naturalAreas.length > 0) {
+        let header = document.createElement('h2');
+        header.className = 'text-center';
+        header.textContent = 'Áreas Naturales';
+        areasContainer.appendChild(header);
+
+        naturalAreas.forEach(area => {
+            let card = document.createElement("div");
+            card.className = "card";
+
+            card.innerHTML = `
+                <img class="card-img w-25" src="../imgs/natural-area.jpg">
+                <div class="card-body p-1 mt-2">
+                    <h5 class="card-name">${area.name}</h5>
+                </div>`;
+
+            areasContainer.appendChild(card);
+        });
+    }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ let searchInput = document.getElementById("searchInput");
 let url = "https://api-colombia.com/"
 let urlDescription = url + "/api/v1/Country/Colombia"
 let urlDepartaments = url + "/api/v1/Department"
-let urlRegions = url + "/api/v1/Region/{id}/departments"
+let urlRegions = url + "/api/v1/Region"
 
 let departments = [];
 let regions = [];
@@ -25,7 +25,7 @@ fetch(urlDescription)
         description.innerHTML = `<p>${res.description}</p>`
     })
     .catch(error => console.error("Error al obtener los datos:", error)
-);
+    );
 
 fetch(urlDepartaments)
     .then(response => response.json())
@@ -34,7 +34,7 @@ fetch(urlDepartaments)
         createCards(departments);
     })
     .catch(error => console.error("Error al obtener los datos:", error)
-);
+    );
 
 fetch(urlRegions)
     .then(response => response.json())
@@ -43,12 +43,11 @@ fetch(urlRegions)
         createCheckboxes();
     })
     .catch(error => console.error("Error al obtener los datos:", error)
-);
-
+    );
 
 function createCards(departments) {
     cardContainer.innerHTML = '';
-    
+
     departments.forEach(department => {
         let card = document.createElement("div");
         card.className = "card";
@@ -56,28 +55,29 @@ function createCards(departments) {
         let cutDescription = cutText(department.description, 100)
 
         card.innerHTML = `
-            <img class="card-img" src="...">
+            <img class="card-img w-25" src="../imgs/img-card.jpg">
             <div class="card-body p-1 mt-2">
                 <h5 class="card-name">${department.name}</h5>
                 <p class="card-text"><span class="fw-bold">Descripción:</span> ${cutDescription}</p>
                 <p class="card-text"><span class="fw-bold">Población:</span> ${department.population}</p>
                 <p class="card-text"><span class="fw-bold">Superficie:</span> ${department.surface}</p>
                 <div class="d-flex justify-content-between align-items-center mb-2">
-                    <a href="./pages/detalles.html?id=${department.id}" class="btn button_card">Details</a>
+                    <a href="./pages/details.html?id=${department.id}" class="btn button_card">Details</a>
                 </div>
             </div>`;
-            cardContainer.appendChild(card);
+
+        cardContainer.appendChild(card);
     });
 }
 
 function createCheckboxes() {
-    checkboxContainer.innerHTML = '';
-
+    checkboxContainer.innerHTML = `<h4>Busca el departamento por Región</h4>`;
+    
     regions.forEach(region => {
         const checkbox = document.createElement('div');
         checkbox.className = 'form-check';
         checkbox.innerHTML = `
-            <input class="form-check-input" type="checkbox" value="${region.name}" id="${region.id}">
+            <input class="form-check-input" type="checkbox" value="${region.id}" id="${region.id}">
             <label class="form-check-label" for="${region.id}"><span class="p_check">${region.name}</span></label>
         `;
         checkboxContainer.appendChild(checkbox);
@@ -89,17 +89,20 @@ function createCheckboxes() {
 }
 
 function filterEvents() {
-    const selectedCategories = Array.from(checkboxContainer.querySelectorAll('input[type="checkbox"]:checked'))
-    .map(checkbox => checkbox.value);
+    const selectedRegionIds = Array.from(checkboxContainer.querySelectorAll('input[type="checkbox"]:checked'))
+        .map(checkbox => checkbox.value);
 
     const searchText = searchInput.value.toLowerCase();
 
     cardContainer.innerHTML = '';
 
     const filteredCards = departments.filter(department => {
-        const isCategoryMatch = selectedCategories.length === 0 || selectedCategories.includes(department.region);
-        const isSearchMatch = department.name.toLowerCase().includes(searchText) || department.description.toLowerCase().includes(searchText);
-        return isCategoryMatch && isSearchMatch;
+        const isRegionMatch = selectedRegionIds.length === 0 || selectedRegionIds.includes(department.regionId.toString());
+        const isSearchMatch = department.name.toLowerCase().includes(searchText) || 
+                              department.description.toLowerCase().includes(searchText) || 
+                              department.population.toString().includes(searchText) || 
+                              department.surface.toString().includes(searchText);
+        return isRegionMatch && isSearchMatch;
     });
 
     if (filteredCards.length === 0) {
@@ -108,8 +111,11 @@ function filterEvents() {
         noResultsMessage.innerHTML = '<H2 class="text-success">No se encontraron resultados.</H2><p class"text-success-emphasis">Por favor intenta con una nueva busqueda.</p>';
         cardContainer.appendChild(noResultsMessage);
     } else {
-        createCards(filteredCards)
+        createCards(filteredCards);
     }
 }
 
 searchButton.addEventListener('click', filterEvents);
+
+
+
